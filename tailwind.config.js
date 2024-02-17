a8d382a41064823eb87aa5404e4e1cfcafc07ebd
Solaimanislam <islamsi2001@gmail.com>
2024-02-18 05:17:38 +0600

    added seat option

---
+++ tailwind.config.js
@@ -0,0 +1,51 @@
+// // // <div class="font-raleway text-center my-auto mx-auto">
+// // <div class="pl-8">
+// // <img class="border-2 border-gray-300 border-dashed" src="images/fare.png" alt="">
+// // </div>
+// // <h1 class=" text-3xl font-bold ">550 Taka</h1>
+// // <p class="text-2xl font-semibold  text-[#03071299]">Per Seat</p>
+
+// // </div>
+
+// <div class=" grid grid-cols-3  ">
+//                         <!-- place -->
+//                         <div class="flex justify-around gap-20 lg:pt-8">
+//                             <div class="text-black flex gap-3 pl-3">
+//                                 <div>
+//                                     <img src="images/bus-logo.png" alt="">
+//                                 </div>
+//                                 <div>
+//                                     <h3 class="font-raleway text-3xl font-semibold">Greenline Paribahan</h3>
+//                                     <p class="font-inter text-lg text-[#03071299]">Coach-009-WEB ! AC_Business</p>
+//                                 </div>
+//                                 <div class="">
+                                    
+//                                 </div>
+//                             </div>
+                            
+//                             <!-- seat -->
+    
+//                             <div>
+//                                 <div class="navbar-end flex ml-14">
+//                                     <a class="btn"><img src="images/seat-green.png" alt="">
+//                                         <p class="text-[#1DD100] lg:font-semibold text-xl font-inter"><span>40 </span>Seats left
+//                                         </p>
+//                                     </a>
+//                                 </div>
+//                                 <div>
+                                    
+//                                 </div>
+//                             </div>
+    
+    
+//                         </div>
+//                         <div>
+
+//                         </div>
+//                         <img src="images/info-devider.png" alt="">
+    
+//                         <!-- tk -->
+                        
+//                     </div>
+//                     <div>
+//                     </div>

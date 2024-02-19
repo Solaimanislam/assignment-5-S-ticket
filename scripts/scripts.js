1f9e82be8f9183cc2d061c61f08d72df0e2970b4
Solaimanislam <islamsi2001@gmail.com>
2024-02-19 22:31:04 +0600

    added final file

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,111 +1,122 @@
 
 let totalPrice = 0;
-let selectedCount = 0; 
+let selectedCount = 0;
 const cards = document.querySelectorAll('.kbd');
 
 for (let i = 0; i < cards.length; i++) {
-  const card = cards[i];
-  
-  card.addEventListener("click", handleClickOnce);
+    const card = cards[i];
+
+    card.addEventListener("click", handleClickOnce);
 }
 
 function handleClickOnce(event) {
-  const card = event.currentTarget;
-  btn.removeAttribute('disabled');
-//   loginBtn.removeAttribute('disabled');
-  if (selectedCount === 4 || card.classList.contains('clicked')) {
-    return; 
-  }
-  
-  card.classList.add('clicked');
-  card.classList.add('bg-lime-500')
-  selectedCount++; 
-  const greenC = document.getElementById('greenCount');
-  greenC.innerText = 40 - selectedCount;
-  
-  const counter = document.getElementById('counter');
-  counter.innerText = selectedCount;
-  const value = card.querySelector('p').innerText;
-  const value1 = 'Economoy';
-  const prices = '550';
-  
-  const titleContainer = document.getElementById('title-container');
-  const p = document.createElement('p');
-  const p1 = document.createElement('p');
-  const p2 = document.createElement('p');
+    const card = event.currentTarget;
+    
+    
+    const inputE = document.getElementById('input-field');
+    if(selectedCount >= 0){
+        
+        number.removeAttribute('disabled');
+        
+    }
+    if (selectedCount === 3) {
+        inputE.removeAttribute('disabled');
+        btn.removeAttribute('disabled');
+        
+    }
+    if (selectedCount === 4 || card.classList.contains('clicked')) {
+        return;
+    }
 
-  p.innerText = value;
-  p1.innerText = value1;
-  p2.innerText = prices;
-  titleContainer.appendChild(p);
-  titleContainer.appendChild(p1);
-  titleContainer.appendChild(p2);
+    card.classList.add('clicked');
+    card.classList.add('bg-lime-500')
+    selectedCount++;
+    const greenC = document.getElementById('greenCount');
+    greenC.innerText = 40 - selectedCount;
 
-  const price = parseFloat(prices);
-  totalPrice += price;
-  document.getElementById('totalPrice').innerText = totalPrice;
+    const counter = document.getElementById('counter');
+    counter.innerText = selectedCount;
+    const value = card.querySelector('p').innerText;
+    const value1 = 'Economoy';
+    const prices = '550';
 
-  card.removeEventListener("click", handleClickOnce);
+    const titleContainer = document.getElementById('title-container');
+    const p = document.createElement('p');
+    const p1 = document.createElement('p');
+    const p2 = document.createElement('p');
+
+    p.innerText = value;
+    p1.innerText = value1;
+    p2.innerText = prices;
+    titleContainer.appendChild(p);
+    titleContainer.appendChild(p1);
+    titleContainer.appendChild(p2);
+
+    const price = parseFloat(prices);
+    totalPrice += price;
+    document.getElementById('totalPrice').innerText = totalPrice;
+
+    card.removeEventListener("click", handleClickOnce);
 }
 
 const btn = document.getElementById('apply-btn');
 
 btn.addEventListener('click', function () {
-    
 
-    
+
+
     const couponElement = document.getElementById('input-field').value;
-    
+
     const couponCode = couponElement.split(" ").join("").toUpperCase();
     const couponCode1 = couponElement;
     console.log(couponCode1);
     console.log(couponCode);
-    if(totalPrice >= 2200){
+    if (totalPrice >= 2200) {
         const btn1 = document.getElementById('apply-btn');
-        
+
         btn1.removeAttribute('disabled');
-        if(couponCode === "NEW15"){
-            
+        if (couponCode === "NEW15") {
+
 
             const discountElement = document.getElementById('discountPrice');
             const discountAmount = totalPrice * 0.15;
             discountElement.innerText = discountAmount;
-            
+
             const restTotal = document.getElementById('total')
             restTotal.innerText = totalPrice - discountAmount;
             document.getElementById('input-field').value = "";
             const hiddenI = document.getElementById('input-hidden');
             hiddenI.classList.add('hidden');
 
-        }else if(couponCode1 === "Couple 20"){
+        } else if (couponCode1 === "Couple 20") {
 
             const discountElement = document.getElementById('discountPrice');
             const discountAmount = totalPrice * 0.2;
             discountElement.innerText = discountAmount;
-            
+
             const restTotal = document.getElementById('total')
             restTotal.innerText = totalPrice - discountAmount;
             const hiddenI = document.getElementById('input-hidden');
             hiddenI.classList.add('hidden');
         }
-        else{
+        else {
             alert('invalid Coupon');
             document.getElementById('input-field').value = "";
         }
     }
-    else{
-        alert ('get 4 tickets');
+    else {
+        alert('get 4 tickets');
         document.getElementById('input-field').value = "";
     }
 });
-  
-const login = document.getElementById('login-form').addEventListener('input', function(){
+
+const login = document.getElementById('login-form').addEventListener('input', function () {
     const userName = document.getElementById('userName').value;
     const number = document.getElementById('number').value;
     const email = document.getElementById('email').value;
     console.log(userName, number);
     const loginBtn = document.getElementById('loginBtn');
-    if(userName.length > 0 && number.length > 0 && email.length > 0){
+    if ( number.length > 0 ) {
         loginBtn.removeAttribute('disabled');
     }
     else {
